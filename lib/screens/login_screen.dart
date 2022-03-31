06d862f17c6f618f34e7d7291932b2526651111e
sleatherv <sleatherv28@hotmail.com>
2022-03-30 21:53:33 -0500

    [ADD] Login Form

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -19,12 +19,54 @@ class LoginScreen extends StatelessWidget {
                     const SizedBox(height: 10),
                     Text('Login', style: Theme.of(context).textTheme.headline4),
                     const SizedBox(height: 10),
-                    const Text('Formulario')
+                    _LoginForm()
                   ],
                 )
-              )
+              ),
+              const SizedBox(height: 50),
+              const Text('Crear una nueva cuenta', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))
             ],
           ),
+        )
+      ),
+    );
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        //Todo: Keep reference to KEY
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                  color: Colors.deepPurple
+                  )
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.deepPurple,
+                    width: 2
+                  )
+                ),
+                hintText: 'john.doe@example.com',
+                labelText: 'Correo electrónico',
+                labelStyle:  TextStyle(
+                  color: Colors.grey
+                ),
+                prefixIcon: Icon(Icons.alternate_email_outlined, color: Colors.deepPurple)
+
+              ),
+            )
+          ],
         )
       ),
     );

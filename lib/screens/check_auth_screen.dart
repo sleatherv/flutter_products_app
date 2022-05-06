3a0872bda381c6cd6ac344fb3bd18362b86021c9
sleatherv <sleatherv28@hotmail.com>
2022-05-05 22:14:30 -0500

    Quit transition between screens

--- lib/screens/check_auth_screen.dart
+++ lib/screens/check_auth_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:products_app/screens/screens.dart';
 import 'package:products_app/services/services.dart';
 import 'package:provider/provider.dart';
 
@@ -18,12 +19,23 @@ class CheckAuthScreen extends StatelessWidget {
                 return const Text('Espere');
               }
 
-              // if(snapshot.data == ''){
-
-              // }
-              Future.microtask(() {
-                Navigator.of(context).pushReplacementNamed('login');
-              });
+              if (snapshot.data == '') {
+                Future.microtask(() {
+                  Navigator.pushReplacement(
+                      context,
+                      PageRouteBuilder(
+                          pageBuilder: (_, __, ___) => const LoginScreen(),
+                          transitionDuration: const Duration(seconds: 0)));
+                });
+              } else {
+                Future.microtask(() {
+                  Navigator.pushReplacement(
+                      context,
+                      PageRouteBuilder(
+                          pageBuilder: (_, __, ___) => const HomeScreen(),
+                          transitionDuration: const Duration(seconds: 0)));
+                });
+              }
 
               return Container();
             }),

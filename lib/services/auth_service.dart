6cfe024a2ca0b49971f6ed6aaedad5e5de7d36d1
sleatherv <sleatherv28@hotmail.com>
2022-05-05 21:28:21 -0500

    Save token in secure storage

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -2,56 +2,62 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
-class AuthService extends ChangeNotifier{
-      
+
+class AuthService extends ChangeNotifier {
   final String _baseUrl = 'identitytoolkit.googleapis.com';
   final String _firebaseToken = dotenv.get('FIREBASE_TOKEN', fallback: '');
 
+  final storage = FlutterSecureStorage();
+
   // If return something is an error.
-  Future<String?> createUser(String email, String password) async{
+  Future<String?> createUser(String email, String password) async {
     final Map<String, dynamic> authData = {
       'email': email,
       'password': password
     };
 
-    final url = Uri.https(_baseUrl, '/v1/accounts:signUp',{
-      "key": _firebaseToken
-    });
+    final url =
+        Uri.https(_baseUrl, '/v1/accounts:signUp', {"key": _firebaseToken});
 
     final resp = await http.post(url, body: json.encode(authData));
 
     final Map<String, dynamic> decodedResp = json.decode(resp.body);
 
-
-      if(decodedResp.containsKey('idToken')){
-        // return decodedResp['idToken'];
-        return null;
-      }else{
-        return decodedResp['error']['message'];
-      }
+    if (decodedResp.containsKey('idToken')) {
+      // return decodedResp['idToken'];
+      await storage.write(key: 'idToken', value: decodedResp['idToken']);
+      return null;
+    } else {
+      return decodedResp['error']['message'];
+    }
   }
-  
-  Future<String?> login(String email, String password) async{
+
+  Future<String?> login(String email, String password) async {
     final Map<String, dynamic> authData = {
       'email': email,
       'password': password
     };
 
-    final url = Uri.https(_baseUrl, '/v1/accounts:signInWithPassword',{
-      "key": _firebaseToken
-    });
+    final url = Uri.https(
+        _baseUrl, '/v1/accounts:signInWithPassword', {"key": _firebaseToken});
 
     final resp = await http.post(url, body: json.encode(authData));
 
     final Map<String, dynamic> decodedResp = json.decode(resp.body);
 
+    if (decodedResp.containsKey('idToken')) {
+      await storage.write(key: 'idToken', value: decodedResp['idToken']);
+      return null;
+    } else {
+      return decodedResp['error']['message'];
+    }
+  }
 
-      if(decodedResp.containsKey('idToken')){
-        // return decodedResp['idToken'];
-        return null;
-      }else{
-        return decodedResp['error']['message'];
-      }
+  Future logout() async {
+    await storage.delete(key: 'idToken');
+
+    return;
   }
 }

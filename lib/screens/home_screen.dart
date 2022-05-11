221fd761e185c4c5024e1662650426b85ca57bbf
sleatherv <sleatherv28@hotmail.com>
2022-05-10 22:21:45 -0500

    [ADD] User Logout

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,37 +6,41 @@ import 'package:products_app/widgets/widgets.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
-
   const HomeScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-
     final productsService = Provider.of<ProductsService>(context);
+    final authService = Provider.of<AuthService>(context, listen: false);
 
-    if(productsService.isLoading) return const LoadingScreen();
+    if (productsService.isLoading) return const LoadingScreen();
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Productos'),
+        leading: IconButton(
+            onPressed: () {
+              authService.logout();
+              Navigator.pushReplacementNamed(context, 'login');
+            },
+            icon: const Icon(Icons.login_outlined)),
       ),
       body: ListView.builder(
         itemCount: productsService.products.length,
         itemBuilder: (context, index) => GestureDetector(
           child: ProductCard(product: productsService.products[index]),
           onTap: () {
-            productsService.selectedProduct = productsService.products[index].copy();
+            productsService.selectedProduct =
+                productsService.products[index].copy();
             Navigator.pushNamed(context, 'product');
           },
         ),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: (){
-          productsService.selectedProduct = Product(
-            available: false,
-            name: '',
-            price: 0);
+        onPressed: () {
+          productsService.selectedProduct =
+              Product(available: false, name: '', price: 0);
           Navigator.pushNamed(context, 'product');
         },
       ),
